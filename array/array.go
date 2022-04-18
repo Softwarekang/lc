d9b091f6eb1009ffb4e541f643ea0c949991dfa7
ankangan <ankangan@tencent.com>
2022-04-18 13:25:05 +0800

    feat(array): twoSum
    添加 twoSum 题解

--- array/array.go
+++ array/array.go
@@ -1,1 +1,15 @@
 package array
+
+// twoSum 题目无特殊要求有对应数值，直接返回对应 index 即可
+func twoSum(nums []int, target int) []int {
+	indexMap := make(map[int]int)
+	for k, v := range nums {
+		num := target - v
+		if index, ok := indexMap[num]; ok {
+			return []int{k, index}
+		}
+		indexMap[v] = k
+	}
+
+	return []int{}
+}
