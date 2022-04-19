caa230c41e0ee09aca8383d30ee6082869f02bf7
ankangan <ankangan@tencent.com>
2022-04-19 21:40:59 +0800

    feat:(array): lengthOfLongestSubstring 题解

--- strings/strings.go
+++ strings/strings.go
@@ -22,3 +22,26 @@ func shortestToChar(s string, c byte) []int {
 	}
 	return res
 }
+
+/*
+tag: 字符串，滑动窗口
+*/
+func lengthOfLongestSubstring(s string) int {
+	// 哈希集合，记录每个字符是否出现过
+	m := map[byte]bool{}
+	n := len(s)
+	rk, ans := 0, 0
+	for i := 0; i < n; i++ {
+		if i != 0 {
+			// 左指针向右移动一格，移除一个字符
+			delete(m, s[i-1])
+		}
+		for rk < n && !m[s[rk]] {
+			// 不断地移动右指针
+			m[s[rk]] = true
+			rk++
+		}
+		ans = max(ans, rk-i)
+	}
+	return ans
+}
