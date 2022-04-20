fc5512656843aa5807fd295cf8539a172fb55ce7
ankangan <ankangan@tencent.com>
2022-04-20 10:03:29 +0800

    feat:(array): minSubArrayLen 题解

--- strings/strings.go
+++ strings/strings.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 /*
 tag: 字符串，双指针
 */
@@ -22,6 +24,9 @@ func shortestToChar(s string, c byte) []int {
 	}
 	return res
 }
+func main() {
+	lengthOfLongestSubstring("abcabcbb")
+}
 
 /*
 tag: 字符串，滑动窗口
@@ -29,19 +34,54 @@ tag: 字符串，滑动窗口
 func lengthOfLongestSubstring(s string) int {
 	// 哈希集合，记录每个字符是否出现过
 	m := map[byte]bool{}
-	n := len(s)
-	rk, ans := 0, 0
-	for i := 0; i < n; i++ {
-		if i != 0 {
-			// 左指针向右移动一格，移除一个字符
-			delete(m, s[i-1])
+	var (
+		left, right int
+	)
+
+	res := math.MinInt
+	bytes := []byte(s)
+	for right < len(bytes) {
+		// 不满足条件，则从左边缩小窗口，找到最后一个符合条件的
+		for m[bytes[right]] {
+			delete(m, bytes[left])
+			left++
 		}
-		for rk < n && !m[s[rk]] {
-			// 不断地移动右指针
-			m[s[rk]] = true
-			rk++
-		}
-		ans = max(ans, rk-i)
+		m[bytes[right]] = true
+		res = max(res, len(m))
+		right++
 	}
-	return ans
+
+	if res == math.MinInt {
+		res = 0
+	}
+	return res
+}
+
+/*
+tag: 数组、滑动窗口
+*/
+func minSubArrayLen(target int, nums []int) int {
+	var (
+		left, right, cur int
+	)
+
+	res := math.MaxInt
+
+	for right < len(nums) {
+		cur += nums[right]
+		/*
+			找到满足条件的集合，从左边缩小窗口，看能不能更小
+		*/
+		for cur >= target {
+			res = min(res, right-left+1)
+			cur -= nums[left]
+			left++
+		}
+		right++
+	}
+
+	if res == math.MaxInt {
+		res = 0
+	}
+	return res
 }
